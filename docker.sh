fc745d27a76e6caac10d140d6cb1fcb5cf1bbe08
Adrian Duong <adrian.duong@gmail.com>
2018-08-22 16:09:39 -0700

    Some docker utils

---
+++ docker.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# removes dangling docker volumes
+rm_dangling_volumes () {
+	local volumes=($(docker volume ls -qf dangling=true))
+	[[ ${#volumes[@]} -gt 0 ]] && docker volume rm "${volumes[@]}"
+}
+
+# removes exited docker containers
+rm_exited_containers () {
+	local containers=($(docker ps -qf status=exited))
+	[[ ${#containers[@]} -gt 0 ]] && docker rm ${containers[@]}
+}
