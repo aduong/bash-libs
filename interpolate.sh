901ca869f78a9ddf7cc1128e7768d6058551ed9d
Adrian Duong <adrian.duong@gmail.com>
2018-09-26 14:58:26 -0700

    Handle stdin and a given file in interpolate

--- interpolate.sh
+++ interpolate.sh
@@ -2,8 +2,9 @@
 
 # Reads from STDIN and interpolates any variables provided by the environment
 interpolate () {
+	local file="${1:-/dev/stdin}"
 	local line
-	perl -pe 's/"/\\"/g' |
+	perl -pe 's/"/\\"/g' "$file" |
 		while IFS='' read -r line; do
 			eval "echo \"$line\""
 		done
