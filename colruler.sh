e132b929285061af5ecbf6e83558a8c0c9d05713
Adrian Duong <adrian.duong@gmail.com>
2018-10-31 00:46:28 -0700

    Output a col(umn) ruler
    Perhaps useful with cut -c

---
+++ colruler.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+cols=$(tput cols)
+
+output_row=0
+for (( c=cols ; c > 0 ; c /= 10 )); do
+	output_row=$((output_row + 1))
+done
+
+while [[ $output_row -gt 0 ]]; do
+	chunk_size=$((10 ** (output_row - 1)))
+	for (( i=0 ; i*chunk_size < cols; i += 1 )); do
+		c=$((cols - i*chunk_size))
+		if [[ $c -gt $chunk_size ]]; then
+			c=$chunk_size
+		fi
+		printf "%-${c}d" $((i % 10))
+	done
+	printf '\n'
+	output_row=$((output_row - 1))
+done
