1321122419010653a76c5921893aaf2124fb1111
Adrian Duong <adrian.duong@gmail.com>
2018-11-13 20:59:37 -0800

    array util

---
+++ arrays.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# array_contains TARGET ELEMS...
+# Succeeds iff TARGET is in ELEMS
+array_contains () {
+	local target=$1
+	shift
+
+	for x in "$@"; do
+		if [[ "$x" == "$target" ]]; then
+			return 0
+		fi
+	done
+	return 1
+}
