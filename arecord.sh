87dd4365915b41a5f311cf5def9e18fd0afedf7d
Adrian Duong <adrian.duong@gmail.com>
2025-10-23 08:56:00 -0700

    Record to flac with `rec`

--- arecord.sh
+++ arecord.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-file=~/recordings/$(date -Iseconds | cut -c-19).wav.xz
+file=~/recordings/$(date -Iseconds | cut -c-19).flac
 echo "Recording to $file"
-arecord -f dat -c 1 -r 48000 | pv -cN in | xz | pv -cN out > "$file"
+rec "$file"
